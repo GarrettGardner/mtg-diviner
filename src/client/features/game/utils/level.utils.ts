b6d3c52e099f8448979cb1c11d6fc46f7dd3e31f
Garrett Gardner <garrettgardner@gmail.com>
2023-08-16 18:16:09 -0400

    forgot the level labels!

--- src/client/features/game/utils/level.utils.ts
+++ src/client/features/game/utils/level.utils.ts
@@ -33,12 +33,12 @@ export const initialLevels: ILevel[] = [
   generateLevel([LEVEL_MODIFIER_KEYS.SET_TYPE_CORE_EXPANSION], {
     number: 1,
     pointsPass: 6,
-    pointsSkip: 18,
+    pointsSkip: 12,
   }),
   generateLevel([LEVEL_MODIFIER_KEYS.SET_TYPE_CORE_EXPANSION], {
     number: 2,
     pointsPass: 8,
-    pointsSkip: 18,
+    pointsSkip: 16,
   }),
   generateLevel([LEVEL_MODIFIER_KEYS.SET_TYPE_CORE_EXPANSION], {
     number: 3,
@@ -60,32 +60,32 @@ export const initialLevels: ILevel[] = [
     pointsPass: 16,
     pointsSkip: 24,
   }),
-  generateLevel([LEVEL_MODIFIER_KEYS.SET_TYPE_CORE_EXPANSION], {
+  generateLevel([LEVEL_MODIFIER_KEYS.ERA_ALL, LEVEL_MODIFIER_KEYS.SET_TYPE_CORE_EXPANSION], {
     number: 7,
     pointsPass: 12,
     pointsSkip: 18,
   }),
-  generateLevel([LEVEL_MODIFIER_KEYS.SET_TYPE_CORE_EXPANSION], {
+  generateLevel([LEVEL_MODIFIER_KEYS.ERA_ALL, LEVEL_MODIFIER_KEYS.SET_TYPE_CORE_EXPANSION], {
     number: 8,
     pointsPass: 14,
     pointsSkip: 20,
   }),
-  generateLevel([LEVEL_MODIFIER_KEYS.SET_TYPE_CORE_EXPANSION], {
+  generateLevel([LEVEL_MODIFIER_KEYS.ERA_ALL, LEVEL_MODIFIER_KEYS.SET_TYPE_CORE_EXPANSION], {
     number: 9,
     pointsPass: 16,
     pointsSkip: 22,
   }),
-  generateLevel([LEVEL_MODIFIER_KEYS.SET_TYPE_CORE_EXPANSION], {
+  generateLevel([LEVEL_MODIFIER_KEYS.ERA_ALL, LEVEL_MODIFIER_KEYS.SET_TYPE_CORE_EXPANSION], {
     number: 10,
     pointsPass: 18,
     pointsSkip: 24,
   }),
-  generateLevel([], {
+  generateLevel([LEVEL_MODIFIER_KEYS.ERA_ALL, LEVEL_MODIFIER_KEYS.SET_TYPE_ALL], {
     number: 11,
     pointsPass: 16,
     pointsSkip: 22,
   }),
-  generateLevel([], {
+  generateLevel([LEVEL_MODIFIER_KEYS.ERA_ALL, LEVEL_MODIFIER_KEYS.SET_TYPE_ALL], {
     number: 12,
     pointsPass: 18,
     pointsSkip: 24,
