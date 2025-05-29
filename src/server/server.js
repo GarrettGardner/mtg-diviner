a18ecad947351d3ef9346a3dd6e1c883450144db
Garrett Gardner <garrettgardner@gmail.com>
2025-05-28 23:33:21 -0400

    remove unnecessary server code

--- src/server/server.js
+++ src/server/server.js
@@ -1,27 +1,11 @@
 const path = require("path");
 const express = require("express");
 
-const { dbpool, MODE, PORT, PUBLIC_URL, GOOGLE_ANALYTICS } = require(path.join(__dirname, "./inc/constants"));
+const { dbpool, PORT } = require(path.join(__dirname, "./inc/constants"));
 
 const app = express();
 
 app.engine("html", require("ejs").renderFile);
-const pjson = require(path.join(__dirname, "../../package.json"));
-
-/* Add Static Files */
-app.use(express.static(path.join(__dirname, "../../public")));
-
-/* Get index */
-app.get("/", (req, res) => {
-  res.render(path.join(__dirname, "/views/index.ejs"), {
-    VERSION: pjson.version,
-    TITLE: "MTG Diviner",
-    DESCRIPTION: "A Magic: The Gathering Card Guessing Game for Streamers",
-    GOOGLE_ANALYTICS: GOOGLE_ANALYTICS,
-    MODE: MODE,
-    PUBLIC_URL: PUBLIC_URL,
-  });
-});
 
 /* API: Get cards */
 app.get("/cards", (req, res) => {
